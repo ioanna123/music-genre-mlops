4c74b1445a34e56dc24984e993c013e7fed2b38c
ioanna123 <ioannapolyxronou@gmail.com>
2024-02-17 17:48:09 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,31 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-deps = [
-    'pydantic==1.10.8',
-    'librosa==0.9.2',
-    'numpy==1.24.3',
-    'ffmpeg-python>=0.2.0',
-    'matplotlib==3.7.1',
-    'torchvision==0.12.0',
-    'torch==1.11.0',
-    'dvc==2.58.2',
-    'smart-open==6.3.0',
-    'mlflow==2.6.0',
-    'optuna==3.3.0'
+deps = {
+    'std': [
+        'pydantic==1.10.8',
+        'librosa==0.9.2',
+        'numpy==1.24.3',
+        'ffmpeg-python>=0.2.0',
+        'matplotlib==3.4.3',  # Corrected version number
+        'torchvision==0.12.0',
+        'torch==1.11.0',
+        'dvc==2.58.2',
+        'smart-open==6.3.0',
+        'mlflow==1.20.0',  # Updated version
+        'optuna==3.3.0'
+    ]
+}
 
-]
-
-setup(name='music-genre-classification',
-      version='1.0.1',
-      description='Music Genre Classification',
-      install_requires=[],
-      extras_require=deps,
-      include_package_data=True,
-      tests_require=[
-      ],
-      setup_requires=[
-      ],
-      entry_points={
-          'console_scripts': [
-              'pipeline = __main__.py:cli'
-          ]
-      }
-      )
+setup(
+    name='music-genre-classification',
+    version='1.0.1',
+    description='Music Genre Classification',
+    install_requires=[],
+    extras_require=deps,
+    entry_points={
+        'console_scripts': [
+            'pipeline = __main__.py:cli'
+        ]
+    },
+    packages=['genre_classification']  # Add your package name here
+)
